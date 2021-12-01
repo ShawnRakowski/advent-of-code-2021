007580eeccbdc42618bcce52afa1a745d3d3ae1b
Shawn Rakowski <shawn.rakowski@tcgplayer.com>
2021-12-01 17:09:50 -0600

    Solve for day 1

---
+++ advent-of-code-2021/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static advent_of_code_2021.Solutions;
+
+Console.WriteLine(D_1_1(File.ReadAllLines("./data/D_1_1/test.txt")));
+Console.WriteLine(D_1_1(File.ReadAllLines("./data/D_1_1/input.txt")));
+
+Console.WriteLine(D_1_2(File.ReadAllLines("./data/D_1_2/test.txt")));
+Console.WriteLine(D_1_2(File.ReadAllLines("./data/D_1_2/input.txt")));
---
+++ advent-of-code-2021/Solutions.cs
@@ -0,0 +1,38 @@
+﻿namespace advent_of_code_2021;
+
+static class Solutions
+{
+    public static string D_1_1(string[] input)
+    {
+        return input
+            .Select(int.Parse)
+            .Aggregate(
+                (Last: int.MaxValue, Count: 0),
+                (acc, curr) => (Last: curr, Count: acc.Count + (curr > acc.Last ? 1 : 0))
+            ).Count.ToString();
+    }
+
+    public static string D_1_2(string[] input)
+    {
+        return D_1_1(input
+            .Select(int.Parse)
+            .SelectMany((v, i) => new[]
+            {
+                (Window: i, Value: v),
+                (Window: i + 1, Value: v),
+                (Window: i + 2, Value: v)
+            })
+            .Where(x => x.Window < input.Length - 2)
+            .Aggregate(
+                new int[input.Length - 2],
+                (acc, curr) => 
+                {
+                    acc[curr.Window] += curr.Value;
+                    return acc;
+                }
+            )
+            .Select(i => i.ToString())
+            .ToArray()
+        );
+    }
+}
