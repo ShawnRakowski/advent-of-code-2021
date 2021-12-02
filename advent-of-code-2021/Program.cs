918a11a62f230abb04062832bd07af7777c87884
Shawn Rakowski <shawn.rakowski@tcgplayer.com>
2021-12-01 23:29:14 -0600

    day 2 solved

--- advent-of-code-2021/Program.cs
+++ advent-of-code-2021/Program.cs
@@ -2,8 +2,14 @@
 
 using static advent_of_code_2021.Solutions;
 
-Console.WriteLine(D_1_1(File.ReadAllLines("./data/D_1_1/test.txt")));
-Console.WriteLine(D_1_1(File.ReadAllLines("./data/D_1_1/input.txt")));
+//Console.WriteLine(D_1_1(File.ReadAllLines("./data/D_1_1/test.txt")));
+//Console.WriteLine(D_1_1(File.ReadAllLines("./data/D_1_1/input.txt")));
 
-Console.WriteLine(D_1_2(File.ReadAllLines("./data/D_1_2/test.txt")));
-Console.WriteLine(D_1_2(File.ReadAllLines("./data/D_1_2/input.txt")));
+//Console.WriteLine(D_1_2(File.ReadAllLines("./data/D_1_2/test.txt")));
+//Console.WriteLine(D_1_2(File.ReadAllLines("./data/D_1_2/input.txt")));
+
+//Console.WriteLine(D_2_1(File.ReadAllLines("./data/D_2_1/test.txt")));
+//Console.WriteLine(D_2_1(File.ReadAllLines("./data/D_2_1/input.txt")));
+
+Console.WriteLine(D_2_2(File.ReadAllLines("./data/D_2_2/test.txt")));
+Console.WriteLine(D_2_2(File.ReadAllLines("./data/D_2_2/input.txt")));
--- advent-of-code-2021/Solutions.cs
+++ advent-of-code-2021/Solutions.cs
@@ -22,4 +22,46 @@ static class Solutions
             .ToArray()
         );
     }
+
+    public static string D_2_1(string[] input)
+    {
+        char DirSet(string direction) =>
+            direction == "forward" ? 'x' : 'y';
+
+        int DeltaMult(string direction) =>
+            direction == "up" ? -1 : 1;
+
+        return input
+            .Select(s => s.Trim().Split())
+            .Select(s => (Direction: s.First(), Delta: int.Parse(s.Last())))
+            .Select(d => (Set: DirSet(d.Direction), Delta: d.Delta * DeltaMult(d.Direction)))
+            .GroupBy(d => d.Set)
+            .Select(g => g.Sum(d => d.Delta))
+            .Aggregate(1, (a, c) => a *= c)
+            .ToString();
+    }
+
+    public static string D_2_2(string[] input)
+    {
+        char DirSet(string direction) =>
+            direction == "forward" ? 'm' : 'a';
+
+        int DeltaMult(string direction) =>
+            direction == "up" ? -1 : 1;
+
+        bool IsMoving(char set) => set == 'm';
+        bool IsAiming(char set) => set == 'a';
+
+        var state = input
+            .Select(s => s.Trim().Split())
+            .Select(s => (Direction: s.First(), Delta: int.Parse(s.Last())))
+            .Select(d => (Set: DirSet(d.Direction), Delta: d.Delta * DeltaMult(d.Direction)))
+            .Aggregate((X: 0, D: 0, A: 0), (a, c) => (
+                X: a.X + (IsMoving(c.Set) ? c.Delta : 0),
+                D: a.D + (IsMoving(c.Set) ? (c.Delta * a.A) : 0),
+                A: a.A + (IsAiming(c.Set) ? c.Delta : 0)
+            ));
+
+        return (state.X * state.D).ToString();
+    }
 }
